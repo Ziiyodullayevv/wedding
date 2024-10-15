c5d6662e32f700bc7ef48f47fbbb04f516dbe390
Ziiyodullayevv <akobirziyod2323@gmail.com>
2024-10-16 00:09:56 +0500

    img

--- src/components/BackgroundMusic.tsx
+++ src/components/BackgroundMusic.tsx
@@ -1,42 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
-
-const MusicOnScroll: React.FC = () => {
-  // Musiqa ijrosi holatini boshqarish uchun state
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  // Audio elementini boshqarish uchun ref
-  const audioRef = useRef<HTMLAudioElement | null>(null);
-
-  // Foydalanuvchi scroll qilganini aniqlaydigan hook
-  useEffect(() => {
-    const handleScroll = () => {
-      if (!isPlaying && audioRef.current) {
-        audioRef.current
-          .play()
-          .then(() => {
-            setIsPlaying(true); // Musiqa ijro etilganini belgilash
-          })
-          .catch((error) => {
-            console.error('Audio ijrosida xatolik:', error);
-          });
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    // Component unmounted bo'lganda event listenerni tozalash
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [isPlaying]);
-
+const BackgroundMusic = () => {
   return (
     <div>
-      {/* Audio elementi */}
-      <audio ref={audioRef} src='../../public/Eminem - Lose Yourself.mp3' />
-      <h1>Scroll qiling va musiqa boshlanadi!</h1>
+      <iframe
+        src='../../public/Eminem - Lose Yourself.mp3'
+        allow='autoplay'
+        id='audio'
+        style={{ display: 'none' }}
+      ></iframe>
     </div>
   );
 };
 
-export default MusicOnScroll;
+export default BackgroundMusic;
