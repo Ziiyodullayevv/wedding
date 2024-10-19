1974b7987204d35ccbc63730fe5ba6d2fe02a35b
Ziiyodullayevv <akobirziyod2323@gmail.com>
2024-10-20 01:14:35 +0500

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
 import './App.css';
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-import BackgroundMusic from './components/BackgroundMusic';
+import CountdownTimer from './components/Timer';
+import TelegramBot from './components/Message';
+import TelegramChatIdFetcher from './components/ChatId';
+
+export const fadeIn = (direction: string, delay: number) => ({
+  hidden: {
+    y: direction === 'up' ? 40 : direction === 'down' ? -40 : 0,
+    opacity: 0,
+    x: direction === 'left' ? 40 : direction === 'right' ? -40 : 0,
+  },
+  show: {
+    y: 0,
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'tween',
+      delay,
+      duration: 0.4,
+      ease: 'easeInOut',
+    },
+  },
+});
 
 function Image({
   id,
   title,
   subtitle,
   subtitle2,
+  names,
+  title2,
+  time,
+  date,
+  manzil,
+  map,
+  message,
 }: {
   id: number;
   title: string;
   subtitle: string;
   subtitle2: string;
+  names: boolean;
+  title2: string;
+  time: boolean;
+  date: string;
+  manzil: string;
+  map: boolean;
+  message: boolean;
 }) {
   const [isLoaded, setIsLoaded] = useState(false);
   const [isInView, setIsInView] = useState(false);
@@ -30,23 +65,94 @@ function Image({
           <div className='overlay'></div>
           <div className='text'>
             <div className='flex flex-col items-center'>
-              <h2 className='ar-gold text-[50px]'>{title}</h2>
-              <h2 className='text-[45px] ar-gold -mt-6'>{subtitle}</h2>
-              <p className='text-base text-ar-gold mt-10'>{subtitle2}</p>
+              <motion.h2
+                variants={fadeIn('up', 0.2)}
+                initial='hidden'
+                whileInView='show'
+                className='ar-gold text-[50px]'
+                viewport={{ once: false, amount: 0.7 }}
+              >
+                {title}
+              </motion.h2>
+              <motion.h2
+                variants={fadeIn('up', 0.3)}
+                initial='hidden'
+                whileInView='show'
+                viewport={{ once: false, amount: 0.7 }}
+                className='text-[45px] ar-gold -mt-6'
+              >
+                {subtitle}
+              </motion.h2>
+              {time && <CountdownTimer />}
+
+              <motion.p
+                variants={fadeIn('up', 0.5)}
+                initial='hidden'
+                whileInView='show'
+                viewport={{ once: false, amount: 0.7 }}
+                className='text-base text-ar-gold mt-10'
+              >
+                {subtitle2}
+              </motion.p>
             </div>
 
+            {map && (
+              <motion.a
+                variants={fadeIn('up', 0.4)}
+                initial='hidden'
+                whileInView='show'
+                viewport={{ once: false, amount: 0.7 }}
+                href='https://yandex.uz/maps/-/CDdRnJir'
+              >
+                <div className='request-loader'>
+                  <span></span>
+                </div>
+              </motion.a>
+            )}
+
+            {message && <TelegramBot />}
+
             <div className='flex flex-col items-center'>
-              <h5 className='flex items-center gap-2 text-sm font-poppins mt-32 uppercase text-gray-200'>
+              <motion.h5
+                variants={fadeIn('up', 0.6)}
+                initial='hidden'
+                whileInView='show'
+                className='flex items-center gap-2 text-sm font-poppins uppercase text-gray-200'
+              >
                 <span className='w-[30px] h-[.5px] inline-block bg-gray-200'></span>
-                <span>wedding of the</span>
+                <span>{title2}</span>
                 <span className='w-[30px] h-[.5px] inline-block bg-gray-200'></span>
-              </h5>
-              <h3 className='ar-gold text-[40px] mt-2 bg-red-500'>
-                Ibrohim vs E'zoza
-              </h3>
-              <h4 className='text-sm font-poppins mt-20 text-yellow-200'>
-                15 Oktober 2024, 18:00 PM
-              </h4>
+              </motion.h5>
+              {names && (
+                <motion.h3
+                  variants={fadeIn('up', 0.7)}
+                  initial='hidden'
+                  whileInView='show'
+                  className='ar-gold text-[40px] mt-2 bg-red-500'
+                >
+                  Ibrohim vs E'zoza
+                </motion.h3>
+              )}
+              {manzil && (
+                <motion.h4
+                  variants={fadeIn('up', 0.6)}
+                  initial='hidden'
+                  whileInView='show'
+                  className='text-[20px] mt-10 text-yellow-200'
+                >
+                  {manzil}
+                </motion.h4>
+              )}
+              {date && (
+                <motion.h5
+                  variants={fadeIn('up', 0.6)}
+                  initial='hidden'
+                  whileInView='show'
+                  className='text-sm font-poppins mt-20 text-gray-200'
+                >
+                  {date}
+                </motion.h5>
+              )}
             </div>
           </div>
         </motion.div>
@@ -63,6 +169,41 @@ export default function App() {
         title='Bizning baxtli'
         subtitle='kunimizga xush kelibsiz!'
         subtitle2=" Biz uchun eng muhim va esda qolarli kun — to'yimiz! Sizni ushbu xursandchilik onlarini biz bilan birgalikda nishonlashga chorlaymiz."
+        names={true}
+        title2='Wedding of the'
+        time={false}
+        date='04 noyabr 2024 16:00 da'
+        manzil=''
+        map={false}
+        message={false}
+      />
+
+      <Image
+        id={2}
+        title='Baxtli kunimizgacha'
+        subtitle=' qolgan lahzalar'
+        subtitle2=''
+        names={false}
+        title2='Manzil'
+        time={true}
+        date=''
+        manzil='Oqdaryo Tumani, "Bahor" toyxonasi'
+        map={true}
+        message={false}
+      />
+
+      <Image
+        id={3}
+        title='Baxtli kunimiz uchun'
+        subtitle='tilaklaringiz!'
+        subtitle2=''
+        names={false}
+        title2='Mezbon'
+        time={false}
+        date=''
+        manzil='Quvondiqovlar va Zokirovlar oilasi'
+        map={false}
+        message={true}
       />
     </>
   );
